f5dfdbabe2b331147d0748da56c6a1c319b54c2d
NikolayKolov <ioman@abv.bg>
2024-07-13 13:49:23 +0300

    fgfdgd

--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -43,7 +43,7 @@ const Navigation = () => {
                     }
                 }}>
                     <Box sx={{ textTransform: 'uppercase' }}>
-                        <NavigationLink text='notes-7' href='/' />
+                        <NavigationLink text='notes-8' href='/' />
                         <AuthComponent>
                             <NavigationLink text='new note' href='/create' cypressTestId="create-new-note" />
                         </AuthComponent>
