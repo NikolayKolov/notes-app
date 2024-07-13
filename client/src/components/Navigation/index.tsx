3164ae7d6205fb210e12318ee833955d635499c9
NikolayKolov <ioman@abv.bg>
2024-07-13 14:55:22 +0300

    ertete

--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -43,7 +43,7 @@ const Navigation = () => {
                     }
                 }}>
                     <Box sx={{ textTransform: 'uppercase' }}>
-                        <NavigationLink text='notes-9' href='/' />
+                        <NavigationLink text='notes-20' href='/' />
                         <AuthComponent>
                             <NavigationLink text='new note' href='/create' cypressTestId="create-new-note" />
                         </AuthComponent>
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -6,8 +6,8 @@ import notesRouter from './notes';
 const mainRouter = express.Router();
 
 // load routes
-mainRouter.use('/api/note', authUserRequest, notesRouter);
-mainRouter.use('/api/user', usersRouter);
+mainRouter.use('/api/note/', authUserRequest, notesRouter);
+mainRouter.use('/api/user/', usersRouter);
 mainRouter.use('/', (_req, res) => {
     res.send('Server running');
 });
