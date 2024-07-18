ddc41165661e619c57f766d732ecb6ea5a7b5dfa
NikolayKolov <ioman@abv.bg>
2024-07-18 16:30:08 +0300

    test url

--- client/src/api/loginUser.ts
+++ client/src/api/loginUser.ts
@@ -6,6 +6,10 @@ const loginUser = async (email: string, password: string): Promise<Response> => 
         password
     };
 
+    console.log('Login User email', email);
+    console.log('Login User backendURL', backendURL);
+    console.log('Login User VITE_BACKEND_URL', import.meta.env.VITE_BACKEND_URL);
+
     const response = await fetch(
         backendURL + '/api/user/login', {
             method: 'POST',
