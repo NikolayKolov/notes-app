759ce21f1bddb33bbc24469c0a614b8e2057bc1f
NikolayKolov <ioman@abv.bg>
2024-07-20 12:03:22 +0300

    fix backend image name

--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -43,7 +43,7 @@ const Navigation = () => {
                     }
                 }}>
                     <Box sx={{ textTransform: 'uppercase' }}>
-                        <NavigationLink text='notes-33' href='/' />
+                        <NavigationLink text='notes' href='/' />
                         <AuthComponent>
                             <NavigationLink text='new note' href='/create' cypressTestId="create-new-note" />
                         </AuthComponent>
